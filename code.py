a0c7cf80531905d43602f99a7eb547fc4ce3eb98
pavan <pavan.m.6360@gmail.com>
2023-08-22 23:54:28 +0530

    first

--- code.py
+++ code.py
@@ -1,1 +1,19 @@
-print('DT PROJECT')
+
+import cv2 as cv
+cap=cv.VideoCapture(0)
+cascade_classifier = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+while True:
+    ret,frame=cap.read()
+    frame=cv.cvtColor(frame,0)
+    detections=cascade_classifier.detectMultiScale(frame)
+
+    if (len(detections)>0):
+        (x,y,w,h)=detections[0]
+        frame=cv.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+
+    cv.imshow('frame',frame)
+    if cv.waitKey(1) & 0xFF ==ord('1'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
